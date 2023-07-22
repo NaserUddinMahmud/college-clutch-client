3ccd39bb5c688366abc44170b0215f697617ae9f
NaserUddinMahmud <mahmudbappy01@gmail.com>
2023-07-22 17:36:46 +0600

    navbar and footer added

---
+++ src/Pages/Home/Home/Home.jsx
@@ -0,0 +1,37 @@
+const Home = () => {
+  return (
+    <div>
+      <h2>hello home</h2>
+      
+      <div className="flex justify-center py-4">
+        <div className="form-control">
+          <div className="input-group">
+            <input
+              type="text"
+              placeholder="Search…"
+              className="input input-accent"
+            />
+            <button className="btn btn-accent">
+              <svg
+                xmlns="http://www.w3.org/2000/sv"
+                className="h-6 w-6"
+                fill="none"
+                viewBox="0 0 24 24"
+                stroke="currentColor"
+              >
+                <path
+                  strokeLinecap="round"
+                  strokeLinejoin="round"
+                  strokeWidth="2"
+                  d="M21 21l-6-6m2-5a7 7 0 11-14 0 7 7 0 0114 0z"
+                />
+              </svg>
+            </button>
+          </div>
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default Home;
---
+++ src/Pages/Shared/Footer.jsx
@@ -0,0 +1,45 @@
+import { Link } from "react-router-dom";
+import logo from '../../assets/logo.png'
+
+const Footer = () => {
+  return (
+    <footer className="footer p-10 bg-base-200 text-base-content">
+      <div>
+      <Link to='/' className=""><img className="h-16" src={logo} alt="" /></Link>
+        <p>
+          ACME Industries Ltd.
+          <br />
+          Providing reliable tech since 1992
+        </p>
+      </div>
+      <div>
+        <span className="footer-title">Services</span>
+        <a className="link link-hover">Branding</a>
+        <a className="link link-hover">Design</a>
+        <a className="link link-hover">Marketing</a>
+        <a className="link link-hover">Advertisement</a>
+      </div>
+      <div>
+        <span className="footer-title">Company</span>
+        <a className="link link-hover">About us</a>
+        <a className="link link-hover">Contact</a>
+        <a className="link link-hover">Jobs</a>
+        <a className="link link-hover">Press kit</a>
+      </div>
+      <div>
+    <span className="footer-title">Newsletter</span> 
+    <div className="form-control w-80">
+      <label className="label">
+        <span className="label-text">Enter your email address</span>
+      </label> 
+      <div className="relative">
+        <input type="text" placeholder="username@site.com" className="input input-bordered w-full pr-16" /> 
+        <button className="btn btn-accent absolute top-0 right-0 rounded-l-none">Subscribe</button>
+      </div>
+    </div>
+  </div>
+    </footer>
+  );
+};
+
+export default Footer;
